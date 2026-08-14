5c2e24fbc89a34b88795e9bd691a36435d769387
vijay <vijay.25b01010780@abes.ac.in>
2026-08-14 11:29:59 +0530

    .

--- 14-08/2.js
+++ 14-08/2.js
@@ -1,3 +1,11 @@
-let sum=0;
-for(let i=1;i<=4;i++,sum+=i);
-console.log(sum);
+// let sum=0;
+// for(let i=1;i<=4;i++,sum+=i);
+// console.log(sum);
+
+let i,j;
+for(i=1;i<=5;i++){
+    for(j=1;j<=i;j++){
+        console.log(i,j);
+    }
+    console.log(""); // Print a new line after each row
+}
