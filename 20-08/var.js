f9c5bf330ef719a1621fa7f6df39d5b1322e7a0e
vijay <vijay.25b01010780@abes.ac.in>
2026-08-20 09:50:04 +0530

    var

---
+++ 20-08/var.js
@@ -0,0 +1,21 @@
+// if(true)
+// {
+//     var x="cse 34";
+//     console.log(x);
+// }
+// console.log(x);
+
+// console.log(x);
+//   let x="cse 34";
+
+function test() {
+    var x = 10;
+    if (true) {
+        var x = 20;
+        console.log(x); // 20
+    }
+    console.log(x); // 20
+}
+
+test();
+console.log(x); // ReferenceError: x is not defined
