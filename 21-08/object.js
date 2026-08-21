6150f0374d0e80b3fcedfc8bba176622e8f37355
vijay <vijay.25b01010780@abes.ac.in>
2026-08-21 11:07:58 +0530

    .

--- 21-08/object.js
+++ 21-08/object.js
@@ -8,10 +8,24 @@
 // person["age"]= 31;
 // console.log(person);
 
-const symbol = Symbol("id");
-const symbol2 = Symbol("id");
-const pp= new Object();
-pp[symbol] = 123;
-pp.name = "John";
-console.log(pp);
+// const symbol = Symbol("id");
+// const symbol2 = Symbol("id");
+// const pp= new Object();
+// pp[symbol] = 123;
+// pp.name = "John";
+// console.log(pp);
 
+let p={
+    name:"vj",
+}
+p={age:20};
+p.dept="cse";
+console.log(p);
+
+const person = {
+  name: "John Doe",
+  age: 30
+};
+// person={name:"vj",age:20};
+person.gender = "male";
+console.log(person);
