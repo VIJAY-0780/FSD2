f00380570c14e6274680e3d8c1f1c5a2aa6b5ae8
vijay <vijay.25b01010780@abes.ac.in>
2026-08-21 11:18:40 +0530

    .

--- 21-08/method.js
+++ 21-08/method.js
@@ -7,15 +7,22 @@
 
 // person.greeting();
 
+// const person = {
+//     name: "vj",
+//     // Method shorthand
+//     greeting() {
+//         console.log("hello " + this.name);
+//     }
+// };
+
+// // Call the method
+// person.greeting(); // hello vj
+
+
 const person = {
     name: "vj",
-    // Method shorthand
-    greeting() {
+    greeting: function() {
         console.log("hello " + this.name);
     }
 };
-
-// Call the method
-person.greeting(); // hello vj
-
-
+person.greeting(); 
