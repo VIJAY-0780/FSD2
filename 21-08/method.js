055afe46a1004d22760b3f9f266c4b77a08b05fc
vijay <vijay.25b01010780@abes.ac.in>
2026-08-21 11:30:31 +0530

    .

--- 21-08/method.js
+++ 21-08/method.js
@@ -27,18 +27,29 @@
 // };
 // person.greeting(); 
 
-const person = {
-    name: "vj",
+// const person = {
+//     name: "vj",
     
-    laptop: {
-        brand1: "Dell",
-        model: "XYZ",
-    },
+//     laptop: {
+//         brand1: "Dell",
+//         model: "XYZ",
+//     },
+// };
+// // DOT NOTATION
+// console.log(person.laptop.brand1); // Dell
+
+// // BRACKET NOTATION
+// console.log(person["laptop"]["model"]); // XYZ
+
+// console.log(person.laptop.brand?.length); // XYZ
+
+const a={
+    a:10,
+    b:20,
 };
-// DOT NOTATION
-console.log(person.laptop.brand1); // Dell
-
-// BRACKET NOTATION
-console.log(person["laptop"]["model"]); // XYZ
-
-console.log(person.laptop.brand1.length); // XYZ
+const b={
+    b:30,
+    c:40,
+};
+//merge a and b
+let c=Object.assign({},a,b);
